9013f91e959efe5d5fec2d6b941e81b151f12bca
apasunoori <anand8533@gmail.com>
2024-07-16 15:42:45 -0400

    1

---
+++ src/test/java/com/nt/bootmvcproj07_requestparambinding/BootMvcProj07RequestParamBindingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.bootmvcproj07_requestparambinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMvcProj07RequestParamBindingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
