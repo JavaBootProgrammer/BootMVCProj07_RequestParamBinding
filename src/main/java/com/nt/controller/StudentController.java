749629ac842b6ac694e61f784cb4e6879961c875
apasunoori <anand8533@gmail.com>
2024-07-17 13:55:47 -0400

    1

--- src/main/java/com/nt/controller/StudentController.java
+++ src/main/java/com/nt/controller/StudentController.java
@@ -13,9 +13,20 @@ public class StudentController {
 		return "welcome";
 	}
 
-	@GetMapping("/register")
-	public String registerStuden(@RequestParam("sno") String sno, @RequestParam("sname") String sname) {
+	// http://localhost:8080/BootMVCProj07_RequestParamBinding/register?sno=1&sname=Anand
 
+	@GetMapping("/register")
+	public String registerStudent(@RequestParam("sno") String sno, @RequestParam("sname") String sname) {
+		System.out.print(sno + "           " + sname);
+		return "register";
+	}
+
+	// //http://localhost:8080/BootMVCProj07_RequestParamBinding/registerDefault?sno=1&sname=Anand
+	// request param name is same as parames so
+	@GetMapping("/registerDefault")
+	public String registerStudentDefault(@RequestParam String sno, @RequestParam String sname) {
+
+		System.out.print(sno + "           " + sname);
 		return "register";
 	}
 
