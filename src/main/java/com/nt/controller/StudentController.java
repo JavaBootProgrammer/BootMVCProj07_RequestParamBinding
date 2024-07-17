b3d1f6839dd7fbf34766e20c114d905e7ab0a7fa
apasunoori <anand8533@gmail.com>
2024-07-17 10:56:37 -0400

    1

--- src/main/java/com/nt/controller/StudentController.java
+++ src/main/java/com/nt/controller/StudentController.java
@@ -1,35 +1,27 @@
 package com.nt.controller;
 
-import java.util.Map;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class StudentController {
 
-
 	@GetMapping("/")
 	public String showHomePage() {
-		
+
 		return "welcome";
 	}
-		
-	
+
 	@GetMapping("/register")
-	public String registerStuden(@RequestParam("sno")String sno, @RequestParam("sname")String sname)
-	{
-			
+	public String registerStuden(@RequestParam("sno") String sno, @RequestParam("sname") String sname) {
+
 		return "register";
 	}
-	
+
 	@GetMapping("/welcome")
-	public String registerStuden()
-	{
-			
+	public String registerStuden() {
+
 		return "welcome";
 	}
 }
---
+++ target/BootMVCProj07_RequestParamBinding-0.0.1/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+	<h1 style="color:red;text-align: center">welcome to Spring Boot MVC application</h1>
+	
+</body>
+</html>
